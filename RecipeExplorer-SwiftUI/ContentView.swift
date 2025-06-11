0c06a2cc9254751b2bd821ffdb5dbae193cb80f0
Lisa <imnotlostrui@aim.com>
2025-06-11 00:31:15 -0400

    Render list with recipe name, picture and cuisine type

--- RecipeExplorer-SwiftUI/ContentView.swift
+++ RecipeExplorer-SwiftUI/ContentView.swift
@@ -8,21 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @ObservedObject private var viewModel = RecipeViewModel()
-    
+    @State private var searchText = ""
+
+    var filteredRecipes: [Recipe] {
+        if searchText.isEmpty {
+            return viewModel.recipes
+        } else {
+            return viewModel.recipes.filter { $0.cuisine.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
     var body: some View {
-        List(viewModel.recipes) { recipe in
-            Text(recipe.name)
+        NavigationView {
+            List(filteredRecipes) { recipe in
+                HStack(alignment: .center) {
+                    VStack(alignment: .center) {
+                        if let url = URL(string: recipe.photo_url_small) {
+                            AsyncImage(url: url) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(height: 100)
+                        }
+                        Text(recipe.cuisine)
+                    }
+                    Text(recipe.name)
+                }
+            }
+            .searchable(text: $searchText, prompt: "Search by cuisine")
+            .task {
+                await viewModel.getRecipes()
+            }
+            .alert("Error", isPresented: .constant(viewModel.errorMessage != nil)) {
+                Button("OK", role: .cancel) {}
+            } message: {
+                Text(viewModel.errorMessage ?? "")
+            }
         }
-        .task {
-            await viewModel.getRecipes()
-        }
-        .alert("Error", isPresented: .constant(viewModel.errorMessage != nil)) {
-            Button("OK", role: .cancel) {}
-        } message: {
-            Text(viewModel.errorMessage ?? "")
-        }
+        .navigationTitle("Recipe Explorer")
     }
 }
 
